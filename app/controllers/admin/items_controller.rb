2179b360f42d6dc7ffc38ba227468dabcad1bcd7
denozo <n.demachi23@gmail.com>
2021-10-28 14:41:31 +0000

    [Add]商品新規登録実行まで作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,12 +9,12 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.genre_id = 
     @item.save
-    redirect_to admin_items
+    redirect_to admin_items_path
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
