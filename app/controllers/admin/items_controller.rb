e91bc454959d43c33ae9d22ec221dd3b436cc88c
denozo <n.demachi23@gmail.com>
2021-10-28 14:21:57 +0000

    [Add]商品新規登録画面作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,17 @@
-class ItemsController < ApplicationController
+class Admin::ItemsController < ApplicationController
+  
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.genre_id = 
+    @item.save
+    redirect_to admin_items
   end
 
   def show
@@ -16,4 +22,11 @@ class ItemsController < ApplicationController
 
   def update
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
+  end
+  
 end
