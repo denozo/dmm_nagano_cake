6c08148ddd27c1796c18a9da8b273f8cf6e99192
denozo <n.demachi23@gmail.com>
2021-11-01 08:37:41 +0000

    [Add]order,order_detailモデル作成アソシエーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,9 @@ class Order < ApplicationRecord
 
   belongs_to :customer
   
+  has_many :order_details
+
+  
   enum payment_method: { credit_card: 0, transfer: 1}
   enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, delivery_preparation: 3, delivered: 4}
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   
 
   namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroyall'
+    get 'cart_items/create'
+  end
+  
+  namespace :public do
     get 'orders/new'
     get 'orders/confirm'
     get 'orders/thanks'
@@ -9,6 +17,7 @@ Rails.application.routes.draw do
     get 'orders/index'
     get 'orders/show'
   end
+  
   namespace :admin do
     root to: 'homes#top'
     resources :genres, only:[:index, :create, :edit, :update]
@@ -20,9 +29,12 @@ Rails.application.routes.draw do
     resources :items, only:[:index, :show]
     resource :customers, only:[:index, :edit, :update, :show]
     resource :addresse, only:[:index, :edit, :create, :update, :destroy]
+    
     root to: 'homes#top'
     get 'home/about', to: 'homes#about'
   end
+  
+  #どの記述が
   
   
   devise_for :admin, controllers: {
