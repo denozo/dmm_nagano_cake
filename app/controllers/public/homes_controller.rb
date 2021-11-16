332fe4c1cc902986e6a11d39fb442e62c7d519ef
denozo <n.demachi23@gmail.com>
2021-11-16 13:32:51 +0000

    商品詳細画面、TOP画面のサイドバーにジャンル検索設置

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,9 @@ class Public::HomesController < ApplicationController
 
   def top
     @items = Item.page(params[:page]).per(4).reverse_order
+    
+    #サイドバーのジャンル検索用にデータを呼び出す
+    @genres = Genre.all
   end
 
   def about
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,18 +2,24 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.page(params[:page]).per(8).reverse_order.search(params[:search])
-    @genres = Genre.all
+    
     #商品一覧に表示する商品の件数を計算
     @itemscount = @items.count
     
     #会員には販売停止中のitemは非表示にする。
     @items = @items.where.not(is_active: '販売停止中')
+    
+    #サイドバーのジャンル検索用にデータを呼び出す
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @customer = current_customer
     @cart_item = CartItem.new
+    
+    #サイドバーのジャンル検索用にデータを呼び出す
+    @genres = Genre.all
   end
 
 
